ea3836b280f0d2dfe8f9d96e8e4a1e8a648a6a7c
erikqb3 <erikqb3@gmail.com>
2023-03-11 20:28:49 -0700

    changing from png to webp

--- javascript/galleryDB.js
+++ javascript/galleryDB.js
@@ -1,131 +1,131 @@
 export const galleryDB = {
     "img1": {
     "id": "01",
-    "imgPath":"./imgs/AR1.png"
+    "imgPath":"./imgs/AR1.webp"
   },
     "img2": {
     "id": "02",
-    "imgPath":"./imgs/AR2.png"
+    "imgPath":"./imgs/AR2.webp"
   },
     "img3": {
     "id": "03",
-    "imgPath":"./imgs/AR3.png"
+    "imgPath":"./imgs/AR3.webp"
   },
     "img4": {
     "id": "04",
-    "imgPath":"./imgs/AR4.png"
+    "imgPath":"./imgs/AR4.webp"
   },
     "img5": {
     "id": "05",
-    "imgPath":"./imgs/AR5.png"
+    "imgPath":"./imgs/AR5.webp"
   },
     "img6": {
     "id": "06",
-    "imgPath":"./imgs/AR6.png"
+    "imgPath":"./imgs/AR6.webp"
   },
   "img7": {
     "id": "07",
-    "imgPath":"./imgs/AR7.png"
+    "imgPath":"./imgs/AR7.webp"
   },
     "img8": {
     "id": "08",
-    "imgPath":"./imgs/AR8.png"
+    "imgPath":"./imgs/AR8.webp"
   },
     "img9": {
     "id": "09",
-    "imgPath":"./imgs/AR9.png"
+    "imgPath":"./imgs/AR9.webp"
   },
     "img10": {
     "id": "10",
-    "imgPath":"./imgs/AR10.png"
+    "imgPath":"./imgs/AR10.webp"
   },
   "img11": {
     "id": "11",
-    "imgPath":"./imgs/AR11.png"
+    "imgPath":"./imgs/AR11.webp"
   },
   "img12": {
     "id": "12",
-    "imgPath":"./imgs/AR12.png"
+    "imgPath":"./imgs/AR12.webp"
   },
   "img13": {
     "id": "13",
-    "imgPath":"./imgs/AR13.png"
+    "imgPath":"./imgs/AR13.webp"
   },
   "img14": {
     "id": "14",
-    "imgPath":"./imgs/AR14.png"
+    "imgPath":"./imgs/AR14.webp"
   },
   "img15": {
     "id": "15",
-    "imgPath":"./imgs/AR15.png"
+    "imgPath":"./imgs/AR15.webp"
   },
   "img16": {
     "id": "16",
-    "imgPath":"./imgs/AR16.png"
+    "imgPath":"./imgs/AR16.webp"
   },
   "img17": {
     "id": "17",
-    "imgPath":"./imgs/AR17.png"
+    "imgPath":"./imgs/AR17.webp"
   },
     "img18": {
     "id": "18",
-    "imgPath":"./imgs/AR18.png"
+    "imgPath":"./imgs/AR18.webp"
   },
   "img19": {
     "id": "19",
-    "imgPath":"./imgs/AR19.png"
+    "imgPath":"./imgs/AR19.webp"
   },
   "img20": {
     "id": "20",
-    "imgPath":"./imgs/AR20.png"
+    "imgPath":"./imgs/AR20.webp"
   },
   "img21": {
     "id": "21",
-    "imgPath":"./imgs/AR21.png"
+    "imgPath":"./imgs/AR21.webp"
   },
   "img22": {
     "id": "22",
-    "imgPath":"./imgs/AR22.png"
+    "imgPath":"./imgs/AR22.webp"
   },
   "img23": {
     "id": "23",
-    "imgPath":"./imgs/AR23.png"
+    "imgPath":"./imgs/AR23.webp"
   },
   "img24": {
     "id": "24",
-    "imgPath":"./imgs/AR24.png"
+    "imgPath":"./imgs/AR24.webp"
   },
   "img25": {
     "id": "25",
-    "imgPath":"./imgs/AR25.png"
+    "imgPath":"./imgs/AR25.webp"
   },
     "img26": {
     "id": "26",
-    "imgPath":"./imgs/AR26.png"
+    "imgPath":"./imgs/AR26.webp"
   },
   "img27": {
     "id": "27",
-    "imgPath":"./imgs/AR27.png"
+    "imgPath":"./imgs/AR27.webp"
   },
   "img28": {
     "id": "28",
-    "imgPath":"./imgs/AR28.png"
+    "imgPath":"./imgs/AR28.webp"
   },
   "img29": {
     "id": "29",
-    "imgPath":"./imgs/AR29.png"
+    "imgPath":"./imgs/AR29.webp"
   },
   "img30": {
     "id": "30",
-    "imgPath":"./imgs/AR30.png"
+    "imgPath":"./imgs/AR30.webp"
   },
   "img31": {
     "id": "31",
-    "imgPath":"./imgs/AR31.png"
+    "imgPath":"./imgs/AR31.webp"
   },
   "img32": {
     "id": "32",
-    "imgPath":"./imgs/AR32.png"
+    "imgPath":"./imgs/AR32.webp"
   },
 }
 
